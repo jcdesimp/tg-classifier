701eb6bddc17d142c227747d0e9cc89ba7b18130
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-05-31 19:35:25 -0400

    Implement initial preprocessing.

--- classifier.py
+++ classifier.py
@@ -1,7 +1,10 @@
 import argparse
+import json
+import nltk
 
 
 def runArgParser():
+  '''parse commandline arguments'''
   parser = argparse.ArgumentParser(description='Train/Test a telegram people classifier.')
   # create subparser
   subparsers = parser.add_subparsers(dest='command', metavar='CMD', help='Train a model, test a model, or classify something.')
@@ -18,8 +21,37 @@ def runArgParser():
 
   return parser.parse_args();
 
-def trainModel():
-  pass
+def preprocessText(textString):
+  '''given a telegram message, preprocess it and return a dictionary
+    {
+      "raw": "The Original Message",
+      "tokens": ['tokenized', 'array', 'of', 'words'],
+      "pos_tags": [('tag','V'),('words','NN')],
+      "token_set": {'bag', 'of', 'unique', 'words'},
+    }
+  '''
+  textData = {}
+  textData['raw'] = textString
+  textData['tokens'] = nltk.tokenize.word_tokenize(textString)
+  textData['pos_tags'] = nltk.pos_tag(textData['tokens'])
+  textData['token_set'] = { x.lower() for x in textData['tokens'] }
+
+  return textData
+
+def extractFeatures(textData):
+  '''extract features from the text data and return a dictionary of features'''
+  features = defaultdict()
+
+
+def trainModel(filename):
+  with open(filename) as f:
+    # process each message
+    for line in f:
+      messageLine = json.loads(line)
+      # only process if text message
+      if messageLine['text']:
+        print(preprocessText(messageLine['text']))
+        return
 
 def testModel():
   pass
@@ -28,9 +60,10 @@ def classifyMessage():
   pass
 
 def main():
+  '''main program execution'''
   args = runArgParser()
   if args.command == 'train':
-    trainModel()
+    trainModel(args.file)
   elif args.command == 'test':
     testMOdel()
   elif args.command == 'class':
